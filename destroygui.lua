c0c585cfa8c821ceb34e34326069a19febee52ed
usernames122 <88596366+usernames122@users.noreply.github.com>
2022-05-21 09:38:43 +0200

    No epilepsy destroygui.lua

--- destroygui.lua
+++ destroygui.lua
@@ -14,11 +14,11 @@ while task.wait() do
 			pcall(function()
 				
 				if obj:IsA("GuiObject")then
-					obj.BackgroundColor3 = BrickColor.random().Color
-					obj.BorderColor3 = BrickColor.random().Color
+					
+							
 					obj:TweenPosition(UDim2.new(math.random(),0,math.random(),0),Enum.EasingDirection.Out,Enum.EasingStyle.Linear)
 				obj.Rotation = math.random(0,360)
-					-- do it all tween
+					
 				end	
 			end)
 		end
